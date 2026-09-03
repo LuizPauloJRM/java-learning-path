2acf530c404e7e50ed1ab0fc7eb500896f6e7081
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2026-09-02 22:54:23 -0300

    Criando Primeiro projeto e saida  no teclado  ,entendendo como funciona a estrutura inicial de um primeiro projeto em Java

---
+++ exercicios/src/fundamentos/PrimeiroProgramaJava.java
@@ -0,0 +1,9 @@
+package fundamentos;
+
+public class PrimeiroProgramaJava {
+	//Main ponto de entrada em um programa java 
+	public static void main(String[]args) {
+		System.out.println("Olá , mundo!");//Imprima na saída do sistema Olá mundo!
+	}
+	
+}
