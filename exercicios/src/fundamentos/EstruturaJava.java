090af80fd6647e4c5f51cfea1430b75d6c83fa9c
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2026-09-24 22:02:03 -0300

    Setença e bloco de código organização

---
+++ exercicios/src/fundamentos/EstruturaJava.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class EstruturaJava {
+	public static void main(String[] args) {
+		//Organizado em setença de código 
+		//O ";" termina uma setença de código 
+		//Um par de chave define um bloco de código 
+		//Método Main porta de entrada da execução do Java 
+		//Um bloco sempre esta dentro de outro 
+		System.out.println("Primeiro programa 01 ");
+		System.out.println("Primeiro programa 02 ");
+	}
+}
